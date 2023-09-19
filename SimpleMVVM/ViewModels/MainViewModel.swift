4dd8e9a22e59a6cc4c0cb44fd85099689f8e0ae0
NikolaiKorotonozhkin <anikola1k@gmail.com>
2023-09-19 21:46:49 +0300

    Initial Commit

---
+++ SimpleMVVM/ViewModels/MainViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewModel.swift
+//  SimpleMVVM
+//
+//  Created by Nikolai  on 18.09.2023.
+//
+
+import Foundation
+
+class MainViewModel {
+    
+    func numberOfSection() -> Int {
+        1
+    }
+    
+    func numbersOfRows(_ section: Int) -> Int {
+        10
+    }
+}
---
+++ SimpleMVVM/Views/MainViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  SimpleMVVM
+//
+//  Created by Nikolai  on 18.09.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    var viewModel = MainViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .white
+        title = "Main Screen"
+        
+        view.addSubview(tableView)
+        setupTableView()
+    }
+
+}
+
+extension MainViewController {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
